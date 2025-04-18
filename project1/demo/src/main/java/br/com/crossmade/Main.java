bbe7c305ee340b60f6eba2ac48b4a863c0d81f68
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-18 10:36:44 -0300

    Initial

---
+++ project1/demo/src/main/java/br/com/crossmade/Main.java
@@ -0,0 +1,10 @@
+package br.com.crossmade;
+
+public class Main {
+    public static void main(String[] args) {
+        var message = """
+                \nThis is a textblock\s\sSomething else happening
+                """;
+        System.out.println(message);
+    }
+}
