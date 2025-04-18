e0c6e6d9a935bbd0b28994291a882272190fe588
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-18 12:05:34 -0300

    variables and data types

---
+++ project2/variables_datatypes/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte houses = 2;
+        short age = 42;
+        int livingDays = 30000;
+        long totalDistanceFromSunInKm = 149_597_870;
+        float weight = 110.4;
+        double price = 891.30;
+        boolean isAlive = true;
+        char sex = 'M';
+        String name = "Gilberto";
+
+    }
+}
